77a76c626ed271febba51ebe3fac711bac657215
Naveen Kharwar <naveenkharwar21@protonmail.com>
2024-02-21 00:23:00 +0530

    💡: fix alignment

--- src/components/HeaderNotice/HeaderNotice.jsx
+++ src/components/HeaderNotice/HeaderNotice.jsx
@@ -7,36 +7,36 @@ import { mediaQueries } from "../../utils/Breakpoints";
 const Notice = styled.div``;
 
 const DesktopNotice = styled.span`
-	color: white;
-	text-transform: uppercase;
-	display: none;
-	justify-content: center;
-
-	${mediaQueries("md")(`
+  color: white;
+  text-transform: uppercase;
+  display: none;
+  justify-content: center;
+  text-align: center;
+  ${mediaQueries("md")(`
 	display: flex;
 `)}
 `;
 
 const MobielNotice = styled(DesktopNotice)`
-	display: flex;
-	${mediaQueries("md")(`
+  display: flex;
+  ${mediaQueries("md")(`
 	display: none;
 `)}
 `;
 
 const HeaderNotice = () => {
-	return (
-		<Container>
-			<Notice>
-				<a href="#">
-					<DesktopNotice>
-						All of your star wars favorites now streaming on disney+
-					</DesktopNotice>
-					<MobielNotice> Stream star wars on disney+ </MobielNotice>
-				</a>
-			</Notice>
-		</Container>
-	);
+  return (
+    <Container>
+      <Notice>
+        <a href="#">
+          <DesktopNotice>
+            All of your star wars favorites now streaming on disney+
+          </DesktopNotice>
+          <MobielNotice> Stream star wars on disney+ </MobielNotice>
+        </a>
+      </Notice>
+    </Container>
+  );
 };
 
 export default HeaderNotice;
